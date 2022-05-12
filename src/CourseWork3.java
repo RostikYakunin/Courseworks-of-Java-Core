e1ed21aaf6e237886ac556771a67403c85aa8cd2
Rostyslav Yakunin <rostishka1310942@gmail.com>
2022-05-12 14:47:52 +0300

    Курсовое задание №3

--- src/CourseWork3.java
+++ src/CourseWork3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CourseWork4 {
+public class CourseWork3 {
 
    static int Method (String str) {
        int sum = 0;                              // создание переменной для подсчета значений букв
